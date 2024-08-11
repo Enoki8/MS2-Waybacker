aa09b9374b05220f0617fe6ae6e2f05cac9eeb14
Enoki <s1f102400471@iniad.org>
2024-08-11 23:51:57 +0900

    整理

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] Player Player;
     public new GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
+        
         camera.transform.position = new Vector3(6.0f, player.transform.position.y-1.0f, camera.transform.position.z);
     }
     // Update is called once per frame
     void LateUpdate()
     {
-        camera.transform.position = new Vector3(camera.transform.position.x, player.transform.position.y-1.0f, camera.transform.position.z);
+        if (Player.uplimit == 0)
+        {
+            camera.transform.position = new Vector3(camera.transform.position.x, player.transform.position.y - 1.0f, camera.transform.position.z);
+        }
     }
 }
