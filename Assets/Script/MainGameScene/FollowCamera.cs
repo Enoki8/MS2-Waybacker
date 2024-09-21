41450d2ebd3e61f0cfd5cd11e1da791e97e66996
Enoki <s1f102400471@iniad.org>
2024-09-21 21:01:33 +0900

    ファイル、フォルダ名整理
    シーン単位でフォルダを分けました

---
+++ Assets/Script/MainGameScene/FollowCamera.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] PlayerController Player;
+    public new GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        camera.transform.position = new Vector3(6.0f, player.transform.position.y-1.0f, camera.transform.position.z);
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Player.uplimit == 0)
+        {
+            camera.transform.position = new Vector3(camera.transform.position.x, player.transform.position.y - 1.0f, camera.transform.position.z);
+        }
+    }
+}
