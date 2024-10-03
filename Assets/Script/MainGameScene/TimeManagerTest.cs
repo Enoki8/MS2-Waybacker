312ca3057831d4370897836d1ca49ea5b4d9527c
Enoki <s1f102400471@iniad.org>
2024-10-03 20:56:38 +0900

    時間制限テスト

---
+++ Assets/Script/MainGameScene/TimeManagerTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TimeManagerTest : MonoBehaviour
+{
+    public int GameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameTime = 100000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameTime--;
+        if (GameTime < 2000)//ピンチ
+        {
+            Debug.Log("WARNING");
+        }
+
+    }
+    public void CountUp(int upcount)
+    { 
+        GameTime += upcount;
+        Debug.Log($"{upcount}回復");
+    }
+}
