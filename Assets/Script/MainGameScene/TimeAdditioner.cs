e3a2fad1322862373e45cce78c04248b3ed9e4f8
St0Re <store1313@outlook.jp>
2024-10-03 20:39:50 +0900

    時間制限機能を追加
    デバッグログに残り時間は出力されます。
    右ctrl長押しで秒数加算されます

---
+++ Assets/Script/MainGameScene/TimeAdditioner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAdditioner : MonoBehaviour
+{   
+    [SerializeField] double addition_time = 100;
+    private double input_time = 0;
+    [SerializeField] double get_keytime = 0.5;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightControl))
+        {
+            if ((input_time += Time.deltaTime) > get_keytime)
+            {
+                Debug.Log("Time_Addition!!!!" + addition_time);
+                TimeManager.static_time += addition_time;
+                input_time = 0;
+            }
+        }
+    }
+}
--- Assets/Script/MainGameScene/TimeManager.cs
+++ Assets/Script/MainGameScene/TimeManager.cs
@@ -1,20 +1,34 @@
-ï»¿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-
 public class TimeManager : MonoBehaviour
 {
-    public float GameTimer;
+    [SerializeField] double time = 100;
+    public static double static_time;
+    double t = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        static_time = time;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (static_time > 0)
+        {
+            if ((t += Time.deltaTime) > 1)
+            {
+                Debug.Log(static_time);
+                t = 0;
+            }
+            static_time -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("‚¨‚í‚è");
+        }
     }
 }
