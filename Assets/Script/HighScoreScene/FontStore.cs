dafd00f1cb5e284cfd7c4a60aea1f4b4c36f7e3f
Enoki <s1f102400471@iniad.org>
2024-10-14 00:00:26 +0900

    ハイスコアとか作ろう
    センス無くて泣く

---
+++ Assets/Script/HighScoreScene/FontStore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontStore : MonoBehaviour
+{
+    [SerializeField] Sprite[] fonts;
+    [SerializeField] Sprite[] number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
