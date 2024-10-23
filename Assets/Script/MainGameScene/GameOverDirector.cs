589b96d0797695e772e0892d63947de41f883d7f
Enoki <s1f102400471@iniad.org>
2024-10-23 14:06:46 +0900

    家帰ったらやる
    よ

--- Assets/Script/MainGameScene/GameOverDirector.cs
+++ Assets/Script/MainGameScene/GameOverDirector.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameOverDirector : MonoBehaviour
 {
     [SerializeField] Director director;
-    [SerializeField] CreateBlock CreateBlock;
+    [SerializeField] CreateBlock createBlock;
+    [SerializeField] DropBlock dropBlock;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,12 @@ public class GameOverDirector : MonoBehaviour
         if (director.getgameover)
         {
             Debug.Log("Gameover");
+            StartCoroutine(TakeScripts());
         }
+    }
+
+    IEnumerator TakeScripts()
+    {
+        yield return new WaitForSeconds(1f);
     }
 }
